718a2b2c18c78caea6734fac1ecc4e287889046b
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-29 15:09:05 +0100

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """flask blueprint module"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """returns a json object of status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """returns a json object containing the count of all classes"""
+    classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+               "Review": "reviews", "State": "states", "User": "users"}
+    dic = {}
+    for key, value in classes.items():
+        dic[value] = storage.count(key)
+    return jsonify(dic)
