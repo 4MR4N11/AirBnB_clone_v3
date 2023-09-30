efc5ee169e50dec95942f3d0d6d786813f66734f
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-09-30 16:59:24 +0100

    Task 10 & 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,14 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 host = getenv('HBNB_API_HOST') or '0.0.0.0'
 port = getenv('HBNB_API_PORT') or '5000'
+
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
